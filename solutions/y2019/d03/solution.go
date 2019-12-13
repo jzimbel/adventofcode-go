57673f217785dead85882311299fced5d3852b40
jzimbel <jzimbel@gmail.com>
2019-12-13 13:39:24 -0500

    Add day 3 part 2

--- solutions/y2019/d03/solution.go
+++ solutions/y2019/d03/solution.go
@@ -8,6 +8,8 @@ import (
 	"github.com/jzimbel/adventofcode-go/solutions"
 )
 
+// dir represents a unit vector in one of the cardinal directions.
+// dir[0] = x component, dir[y] = y component
 type dir [2]int
 
 type point struct {
@@ -15,77 +17,106 @@ type point struct {
 	y int
 }
 
-type vec struct {
-	dir dir
-	mag int
+// record of whether a point has been visited and how long the path was when it was first visited.
+type record struct {
+	visited    bool
+	pathLength int
 }
 
-type drawingGrid struct {
-	c point          // cursor
-	g map[point]bool // grid
+// cursor records a point and the path distance traveled to reach that point.
+type cursor struct {
+	p    point
+	dist int
+}
+
+// grid holds a map that records which points have been visited by which paths,
+// as well as cursors for the two paths.
+type grid struct {
+	g map[point][2]record
+	c [2]cursor
 }
 
 var (
 	origin = point{}
-	dirs   = map[byte]dir{
-		'D': dir{0, 1},
-		'L': dir{-1, 0},
-		'R': dir{1, 0},
-		'U': dir{0, -1},
+	dirs   = map[byte]*dir{
+		'D': &dir{0, 1},
+		'L': &dir{-1, 0},
+		'R': &dir{1, 0},
+		'U': &dir{0, -1},
 	}
 )
 
-func newVec(descriptor string) *vec {
-	mag, _ := strconv.Atoi(descriptor[1:])
-	return &vec{
-		dir: dirs[descriptor[0]],
-		mag: mag,
+func newGrid() *grid {
+	return &grid{
+		make(map[point][2]record),
+		[2]cursor{},
 	}
 }
 
-func newDrawingGrid() *drawingGrid {
-	return &drawingGrid{g: make(map[point]bool)}
-}
-
-func (g *drawingGrid) draw(v *vec) {
-	xInc, yInc := v.dir[0], v.dir[1]
-	x, y := g.c.x+xInc, g.c.y+yInc
-	for i := 0; i < v.mag; i, x, y = i+1, x+xInc, y+yInc {
-		g.g[point{x, y}] = true
+// draw a new point on the grid for the given wire by moving that wire's cursor in the given direction.
+func (g *grid) draw(d *dir, wireNum int) {
+	g.c[wireNum].p.x += d[0]
+	g.c[wireNum].p.y += d[1]
+	g.c[wireNum].dist++
+	records, ok := g.g[g.c[wireNum].p]
+	if ok {
+		if !records[wireNum].visited {
+			records[wireNum].visited = true
+			records[wireNum].pathLength = g.c[wireNum].dist
+			g.g[g.c[wireNum].p] = records
+		}
+	} else {
+		records = [2]record{}
+		records[wireNum].visited = true
+		records[wireNum].pathLength = g.c[wireNum].dist
+		g.g[g.c[wireNum].p] = records
 	}
-	g.c = point{x - xInc, y - yInc}
 }
 
-func (g *drawingGrid) intersect(g2 *drawingGrid) (shared []point) {
-	for p1 := range g.g {
-		if _, ok := g2.g[p1]; ok {
-			shared = append(shared, p1)
+// draw a full wire on the grid based on the given move set.
+func (g *grid) drawPath(moveSet []*dir, wireNum int) {
+	for stepNum := range moveSet {
+		g.draw(moveSet[stepNum], wireNum)
+	}
+}
+
+// intersections finds all points in the grid where the wires crossed,
+// and returns a slice of cursors giving the intersection points and
+// the total distances traveled by the wires at the time they crossed.
+func (g *grid) intersections() []cursor {
+	shared := make([]cursor, 0, len(g.g))
+	for p, records := range g.g {
+		if records[0].visited && records[1].visited {
+			shared = append(shared, cursor{p, records[0].pathLength + records[1].pathLength})
 		}
 	}
-	return
+	return shared
 }
 
-func getSteps(input string) [][]*vec {
-	steps := make([][]*vec, 2)
+// getMoves decomposes the input into slices of 1-step movements.
+func getMoves(input string) [][]*dir {
 	wires := strings.Split(input, "\n")
+	moves := make([][]*dir, len(wires))
+
 	for i, wire := range wires {
-		descriptors := strings.Split(wire, ",")
-		for _, d := range descriptors {
-			steps[i] = append(steps[i], newVec(d))
+		vecs := strings.Split(wire, ",")
+
+		for _, vec := range vecs {
+			mag, _ := strconv.Atoi(vec[1:])
+			d := dirs[vec[0]]
+			stroke := make([]*dir, mag)
+
+			for j := 0; j < mag; j++ {
+				stroke[j] = d
+			}
+
+			moves[i] = append(moves[i], stroke...)
 		}
 	}
-	return steps
+	return moves
 }
 
-func drawPath(c chan *drawingGrid, steps []*vec) {
-	defer close(c)
-	grid := newDrawingGrid()
-	for _, step := range steps {
-		grid.draw(step)
-	}
-	c <- grid
-}
-
+// manhattan calculates the Manhattan distance between two points.
 func manhattan(p1 point, p2 point) int {
 	return int(
 		math.Abs(float64(p1.x-p2.x)) +
@@ -93,28 +124,40 @@ func manhattan(p1 point, p2 point) int {
 	)
 }
 
-func part1(steps [][]*vec) (minDist int) {
-	c1, c2 := make(chan *drawingGrid), make(chan *drawingGrid)
-	go drawPath(c1, steps[0])
-	go drawPath(c2, steps[1])
-	grid1 := <-c1
-	grid2 := <-c2
-	crosses := grid1.intersect(grid2)
-	minDist = manhattan(origin, crosses[0])
-	for _, p := range crosses[1:] {
-		if dist := manhattan(origin, p); dist < minDist {
+func part1(moves [][]*dir) (minDist int) {
+	g := newGrid()
+	for wireNum := range moves {
+		g.drawPath(moves[wireNum], wireNum)
+	}
+
+	crosses := g.intersections()
+	minDist = manhattan(origin, crosses[0].p)
+	for _, c := range crosses[1:] {
+		if dist := manhattan(origin, c.p); dist < minDist {
 			minDist = dist
 		}
 	}
 	return
 }
 
-func part2(steps [][]*vec) (stepCount int) {
+func part2(moves [][]*dir) (minPathLength int) {
+	g := newGrid()
+	for wireNum := range moves {
+		g.drawPath(moves[wireNum], wireNum)
+	}
+
+	crosses := g.intersections()
+	minPathLength = crosses[0].dist
+	for _, c := range crosses[1:] {
+		if c.dist < minPathLength {
+			minPathLength = c.dist
+		}
+	}
 	return
 }
 
 // Solve provides the day 3 puzzle solution.
 func Solve(input string) (*solutions.Solution, error) {
-	steps := getSteps(input)
-	return &solutions.Solution{Part1: part1(steps), Part2: part2(steps)}, nil
+	moves := getMoves(input)
+	return &solutions.Solution{Part1: part1(moves), Part2: part2(moves)}, nil
 }
