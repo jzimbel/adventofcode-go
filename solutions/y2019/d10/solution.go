32cea84ec272a55bb9c901c680289a27425d0d8c
jzimbel <jzimbel@gmail.com>
2019-12-22 04:09:10 -0500

    Add day 10 part 2 solution

--- solutions/y2019/d10/solution.go
+++ solutions/y2019/d10/solution.go
@@ -2,6 +2,7 @@ package d10
 
 import (
 	"math"
+	"sort"
 	"strings"
 
 	"github.com/jzimbel/adventofcode-go/solutions"
@@ -12,6 +13,8 @@ const (
 	width  = 24
 	height = 24
 )
+
+var epsilon float64
 
 type point struct {
 	x int
@@ -48,37 +51,132 @@ func memoizedGCD() (gcd func(int, int) int) {
 	return
 }
 
-// axisDistances returns the x and y distances between two points.
-func axisDistances(p1, p2 point) (int, int) {
-	x, y := int(math.Abs(float64(p1.x-p2.x))), int(math.Abs(float64(p1.y-p2.y)))
-	return x, y
+// axisDistances returns (separately) the x and y distances between two points.
+func axisDistances(p1, p2 *point) (xDist int, yDist int) {
+	xDist, yDist = int(math.Abs(float64(p1.x-p2.x))), int(math.Abs(float64(p1.y-p2.y)))
+	return
 }
 
-func part1(g grid, gcd func(int, int) int) (maxVisibleCount int) {
-	for p := range g {
+func isBlocked(p1, p2 *point, g grid, gcd func(int, int) int) (blocked bool) {
+	denom := gcd(axisDistances(p1, p2))
+	xStepSize, yStepSize := (p2.x-p1.x)/denom, (p2.y-p1.y)/denom
+	for i := 1; i < denom; i++ {
+		if _, ok := g[point{p1.x + i*xStepSize, p1.y + i*yStepSize}]; ok {
+			blocked = true
+			break
+		}
+	}
+	return
+}
+
+func part1(g grid, gcd func(int, int) int) (maxVisibleCount int, optimalPoint point) {
+	for p1 := range g {
 		var visibleCount int
-		for other := range g {
-			if p == other {
+		for p2 := range g {
+			if p1 == p2 {
 				continue
 			}
-			denom := gcd(axisDistances(p, other))
-			xStepSize, yStepSize := (other.x-p.x)/denom, (other.y-p.y)/denom
-			var blocked bool
-			for i := 1; i < denom; i++ {
-				if _, ok := g[point{p.x + i*xStepSize, p.y + i*yStepSize}]; ok {
-					blocked = true
-					break
-				}
-			}
-			if !blocked {
+			if !isBlocked(&p1, &p2, g, gcd) {
 				visibleCount++
 			}
 		}
 		if visibleCount > maxVisibleCount {
 			maxVisibleCount = visibleCount
+			optimalPoint = p1
 		}
 	}
 	return
+}
+
+// getCenteredGrid creates and returns a new grid with all
+// points translated such that p at the origin.
+func getCenteredGrid(g grid, p point) (cg grid) {
+	cg = make(grid, len(g))
+	for p2 := range g {
+		cg[point{p2.x - p.x, p2.y - p.y}] = struct{}{}
+	}
+	return cg
+}
+
+type rPoint struct {
+	r    float64
+	θ    float64 // #codegolfing
+	orig point
+}
+
+// rPoints is an ordered list of radial (or polar) coordinates.
+// Polar axis (where θ = 0) is up.
+type rPoints []rPoint
+
+func (rg rPoints) Len() int {
+	return len(rg)
+}
+
+func (rg rPoints) Less(i, j int) bool {
+	if math.Abs(rg[i].θ-rg[j].θ) < epsilon {
+		return rg[i].r < rg[j].r
+	}
+	return rg[i].θ < rg[j].θ
+}
+
+func (rg rPoints) Swap(i, j int) {
+	rg[i], rg[j] = rg[j], rg[i]
+}
+
+// dist returns the Euclidean distance between two points. ( sqrt(a**2 + b**2) )
+func dist(p1, p2 *point) float64 {
+	return math.Sqrt(math.Pow(math.Abs(float64(p1.x-p2.x)), 2) + math.Pow(math.Abs(float64(p1.y-p2.y)), 2))
+}
+
+// clockwiseAngleFromUp calculates the angle from -y in radians, moving clockwise.
+// Atan2 normally takes arguments as (y,x), but we reverse them and negate x in order to
+// have polar θ = 0 be Cartesian (0,-1) and increasing θ move in a clockwise direction.
+// Atan2 also produces values in range [-π, π], but we want them to be [0, 2π],
+// so when it would normally produce a negative, we use 2π + atan2Result.
+func clockwiseAngleFromUp(p *point) (θ float64) {
+	newX, newY := -p.y, p.x
+	θ = math.Atan2(float64(newY), float64(newX))
+	if θ < 0 {
+		θ = 2*math.Pi + θ
+	}
+	return
+}
+
+func part2(cg grid, gcd func(int, int) int, optimalPoint point) int {
+	// record angle and distance from center of each asteroid in a sorted slice of struct {rad float64; dist float64}
+	rp := make(rPoints, 0, len(cg))
+	origin := point{}
+	for p := range cg {
+		if p == origin {
+			continue
+		}
+		rp = append(rp, rPoint{r: dist(&origin, &p), θ: clockwiseAngleFromUp(&p), orig: p})
+	}
+	sort.Sort(rp)
+
+	var vaporizedCount int
+	for len(rp) > 0 {
+		nextRp := make(rPoints, 0, len(rp))
+		remove := make([]*point, 0, len(rp))
+		for i := range rp {
+			if isBlocked(&origin, &rp[i].orig, cg, gcd) {
+				nextRp = append(nextRp, rp[i])
+			} else {
+				vaporizedCount++
+				if vaporizedCount == 200 {
+					return (optimalPoint.x+rp[i].orig.x)*100 + (optimalPoint.y + rp[i].orig.y)
+				}
+				remove = append(remove, &rp[i].orig)
+			}
+		}
+
+		for i := range remove {
+			delete(cg, *remove[i])
+		}
+		rp = nextRp
+	}
+	// unreachable as long as there are at least 200 asteroids on the grid
+	return 0
 }
 
 // Solve provides the day 10 puzzle solution.
@@ -94,5 +192,13 @@ func Solve(input string) (*solutions.Solution, error) {
 	}
 	gcd := memoizedGCD()
 
-	return &solutions.Solution{Part1: part1(g, gcd), Part2: nil}, nil
+	maxVisibleCount, optimalPoint := part1(g, gcd)
+
+	// now center the grid on the optimal point for part 2
+	g = getCenteredGrid(g, optimalPoint)
+	return &solutions.Solution{Part1: maxVisibleCount, Part2: part2(g, gcd, optimalPoint)}, nil
+}
+
+func init() {
+	epsilon = math.Nextafter(1, 2) - 1
 }
